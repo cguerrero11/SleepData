b49749ffdea78f4ca27533c48e28f59c65ce806a
Chris <cguerrero1@my.wctc.edu>
2021-02-07 22:46:14 -0600

    edited sleepdata TODO

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace SleepData
     {
         static void Main(string[] args)
         {
+            var file = "data.txt";
             // ask for input
             Console.WriteLine("Enter 1 to create data file.");
             Console.WriteLine("Enter 2 to parse data.");
@@ -55,7 +56,32 @@ namespace SleepData
             }
             else if (resp == "2")
             {
-                // TODO: parse data file
+                 StreamReader sr = new StreamReader(file);
+
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        string[] weeks = line.Split(",");
+
+                        //weeks[0] is date, weeks[1] is hours slept [7]
+                        DateTime date = DateTime.Parse(weeks[0]);
+                        // string[] hours = weeks[1].Split("|");
+                        int[] hours = Array.ConvertAll(weeks[1].Split('|'), int.Parse);
+
+                        // Console.WriteLine($"Week of {0:MMM}, {0:dd}, {0:yyyy}", date);
+                        Console.WriteLine($"Week of {date:MMM}, {date:dd}, {date:yyyy}");
+
+                        // Console.WriteLine("  Su Mo Tu We Th Fr Sa");
+                        Console.WriteLine($"{"Su",3}{"Mo",3}{"Tu",3}{"We",3}{"Th",3}{"Fr",3}{"Sa",3}"); 
+                        Console.WriteLine($"{"--",3}{"--",3}{"--",3}{"--",3}{"--",3}{"--",3}{"--",3}"); 
+                        
+                        Console.WriteLine($"{hours[0],3}{hours[1],3}{hours[2],3}{hours[3],3}{hours[4],3}{hours[5],3}{hours[6],3}"); // {hours.Sum(),4}{Math.Round(hours.Sum() / 7.0, 1),4:n1}");
+                        Console.WriteLine();
+
+                        
+
+
+                    }
 
             }
         }
